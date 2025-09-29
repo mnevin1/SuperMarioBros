0f61d9540074b7726eb0cad82e983dd37718bf4d
Mark Nevin <mnevin1@my.wctc.edu>
2025-09-29 14:41:18 -0500

    Create character class

---
+++ Character.cs
@@ -0,0 +1,15 @@
+class Character
+{
+    public UInt64 Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public string Species { get; set; } = string.Empty;
+    public string FirstAppearance { get; set; } = string.Empty; 
+    public int YearCreated { get; set; }
+
+    public string Display()
+    {
+        return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecies: {Species}\nFirst Appearance: {FirstAppearance}\nYear Created: {YearCreated}\n";
+    }
+}
